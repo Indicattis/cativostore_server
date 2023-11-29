ab5e036a7d2d0ce65a7b5fb4b2e0116e7874d1ce
Indicattis <joao.staehler@gmail.com>
2023-11-29 14:29:11 -0300

    BE - Created
    Product Update System

---
+++ api/products/product/get.js
@@ -0,0 +1,16 @@
+
+const express = require ('express');
+const db = require ('../../../db.js');
+
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = `SELECT * FROM products WHERE id = ?`;
+    
+    db.query(sql, [id], (err, result) => {
+        if (err) return res.json({ Message: "Error inside server" });
+        return res.json(result);
+    });
+});
+module.exports = router;
--- index.js
+++ index.js
@@ -20,7 +20,33 @@ app.get('/products', (req, res) => {
         return res.json(result);
     });
 })
+app.get('/product/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = `SELECT * FROM products WHERE id = ?`;
+    
+    db.query(sql, [id], (err, result) => {
+        if (err) return res.json({ Message: "Não foi possível consultar o produto" });
+        return res.json(result);
+    });
+});
 
+app.put('/product_update/:id', (req, res) => {
+    const id = req.params.id;
+    const { title, description, price, offer, status } = req.body;
+
+    const sqlUpdate = `UPDATE products
+                       SET title = ?, description = ?, price = ?, offer = ?, status = ?
+                       WHERE id = ?`;
+
+    db.query(sqlUpdate, [title, description, price, offer, status, id], (err, result) => {
+        if (err) {
+            console.error("Erro ao atualizar produto:", err);
+            return res.status(500).json({ error: "Erro ao atualizar produto" });
+        }
+
+        return res.json({ message: "Produto atualizado com sucesso!" });
+    });
+});
 
 app.listen(port, () => {
     console.log("Server is running on port "+ port)
