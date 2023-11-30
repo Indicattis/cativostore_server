549c6e5b23bfa33a788013b8c2fcffcfb0326998
Indicattis <joao.staehler@gmail.com>
2023-11-29 21:21:18 -0300

    BE - Create
    Product insert
    Product delete

--- index.js
+++ index.js
@@ -48,6 +48,37 @@ app.put('/product_update/:id', (req, res) => {
     });
 });
 
+app.post('/product_insert', (req, res) => {
+    const { title, description, price, offer, status } = req.body;
+
+    const sql = `INSERT INTO products (title, description, price, offer, status)
+    VALUES (?, ?, ?, ?, ?)`;
+
+    db.query(sql, [title, description, price, offer, status], (err, result) => {
+        if (err) {
+            console.error("Erro ao cadastrar produto:", err);
+            return res.status(500).json({ error: "Erro ao cadastrar produto" });
+        }
+
+        return res.json({ message: "Produto cadastrado com sucesso!" });
+    });
+});
+
+app.post('/product_delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    const sql = `DELETE FROM products WHERE id = ?`;
+
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            console.error("Erro ao deletar produto:", err);
+            return res.status(500).json({ error: "Erro ao deletar produto" });
+        }
+
+        return res.json({ message: "Produto deletado com sucesso!" });
+    });
+});
+
 app.listen(port, () => {
     console.log("Server is running on port "+ port)
 })
